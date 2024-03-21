92e1197ef0a3060a149fcefa5c666ff711a2f0d6
xiaoy <1103098607@qq.com>
2024-03-21 16:56:14 +0800

    period commit;

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/abrance/something/pkg/gen_host"
 	"log"
 )
@@ -10,13 +11,27 @@ func main() {
 }
 
 func caseGenHost() {
-	entries := map[string]string{
-		"127.0.0.1":   "localhost",
-		"192.168.1.1": "example.com",
-		"12.3.1.23":   "HOST1",
-		"123.12.3.12": "HOST2",
-		// 添加更多的 IP-Hostname 映射
+	nh1 := gen_host.NodeHost{
+		IpLs: []string{"192.168.1.2", "192.168.1.1"},
+		TyId: gen_host.HostnameTypeM,
 	}
+	nh2 := gen_host.NodeHost{
+		IpLs: []string{"172.16.0.1"},
+		TyId: gen_host.HostnameSeByNodeId,
+	}
+	nh3 := gen_host.NodeHost{
+		IpLs: []string{"172.16.0.10"},
+		TyId: gen_host.HostnameSeByNodeId,
+	}
+	scHost := gen_host.NodeHost{
+		IpLs: []string{"172.24.0.1"},
+		TyId: gen_host.HostnameSC,
+	}
+	nh2.SetSeNodeId("$nodeid2")
+	nh3.SetSeNodeId("$nodeid3")
+
+	entries := gen_host.GenerateEntries([]gen_host.NodeHost{nh1, nh2, nh3, scHost})
+	fmt.Println(entries)
 	err := gen_host.GenerateHostsFile(entries, "./hosts")
 	if err != nil {
 		log.Fatal(err)
--- pkg/gen_host/gen_host.go
+++ pkg/gen_host/gen_host.go
@@ -3,6 +3,7 @@ package gen_host
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"sort"
 )
@@ -12,13 +13,32 @@ type kv struct {
 	Value string
 }
 
+// GenerateHostsFile 生成 hosts 文件
+// 生成后的 文件如下，已做了 hostname 排序
+// 127.0.0.1	localhost
+// ::1	ip6-localhost ip6-loopback
+// fe00::0	ip6-localnet
+// ff00::0	ip6-mcastprefix
+// ff02::1	ip6-allnodes
+// ff02::2	ip6-allrouters
+//
+// 172.16.0.1	$nodeid2.srhino.svc.local
+// 172.16.0.10	$nodeid3.srhino.svc.local
+// 172.24.0.1	sc.srhino.svc.local
+// 192.168.1.2	sem.srhino.svc.local
+// 192.168.1.1	sem.srhino.svc.local
 func GenerateHostsFile(entries map[string]string, hostFile string) error {
 	// 打开文件
 	file, err := os.OpenFile(hostFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
 		return err
 	}
-	defer file.Close()
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
 
 	writer := bufio.NewWriter(file)
 	var ih = [6][2]string{
@@ -56,4 +76,61 @@ func GenerateHostsFile(entries map[string]string, hostFile string) error {
 		}
 	}
 	return writer.Flush()
+}
+
+const (
+	HostnameTypeM      = "sem.srhino.svc.local"
+	HostnameSeByNodeId = "srhino.svc.local"
+	HostnameSC         = "sc.srhino.svc.local"
+)
+
+type NodeHost struct {
+	TyId     string
+	IpLs     []string
+	seNodeId string // 引擎才有
+}
+
+func (s *NodeHost) SetIps(ipLs []string) {
+	s.IpLs = ipLs
+}
+
+func (s *NodeHost) SetSeNodeId(seNodeId string) {
+	s.seNodeId = seNodeId
+}
+
+func (s *NodeHost) SetType(tyId string) error {
+	switch tyId {
+	case HostnameTypeM, HostnameSeByNodeId, HostnameSC:
+		s.TyId = tyId
+		return nil
+	default:
+		return fmt.Errorf("unsupported type Id: %s", tyId)
+	}
+}
+
+func (s *NodeHost) InsertIntoEntries(entries map[string]string) {
+	switch s.TyId {
+	case HostnameSC, HostnameTypeM:
+		for _, ip := range s.IpLs {
+			entries[ip] = s.TyId
+		}
+	case HostnameSeByNodeId:
+		for _, ip := range s.IpLs {
+			entries[ip] = fmt.Sprintf("%s.%s", s.seNodeId, s.TyId)
+		}
+	default:
+		log.Fatal("unsupported type Id: %s", s.TyId)
+		return
+	}
+}
+
+// GenerateEntries
+// hostnameMIpLs : 管理面 ip 列表
+// scHostIp : 总控 ip 列表 ，考虑后面做总控修改 ip
+func GenerateEntries(nodeHostLs []NodeHost) map[string]string {
+	entries := make(map[string]string)
+	for _, node := range nodeHostLs {
+		node.InsertIntoEntries(entries)
+	}
+	return entries
 }
